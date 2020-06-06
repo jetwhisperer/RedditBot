14e3d2c1071be3f3102a25448f79f3ffd93daaea
Daniel <56766343+jetwhisperer@users.noreply.github.com>
2020-06-06 09:43:39 -0400

    LoggedIDs in a set, combined submission checks, skip all previously seen posts

--- RedditBot.py
+++ RedditBot.py
@@ -10,97 +10,78 @@ from datetime import datetime
 import passwords as secrets
 from twilio.rest import Client
 
-def SendTextMessage(recipient,message):
+
+def SendTextMessage(recipient, message):
     accountSID = '<Your SID from Twilio>'
     authToken = '<Your Twilio Authentication Token>'
-    twilioCli = Client(accountSID,authToken)
+    twilioCli = Client(accountSID, authToken)
     myTwilioNumber = '<Your Twilio Phone NUmber>'
-    
+
     twilioCli.messages.create(body=message,
-                                    from_=myTwilioNumber, to=recipient)
+                              from_=myTwilioNumber, to=recipient)
+
 
 reddit = praw.Reddit(client_id='<REDDIT CLIENT ID>',
                      client_secret='<REDDIT SECRET>',
                      password='<REDDIT PASSWORD>',
-                    user_agent='Some creative tagline here u/YOU',
-                    username='<REDDIT USERNAME')
+                     user_agent='Some creative tagline here u/YOU',
+                     username='<REDDIT USERNAME')
 
 reddit.read_only = True
 
-#enter the sub, then a list for the items you are looking for in the sub
-#any deal in a 'swap' reddit will only look in the HAVE section of the title
-deals  = {
-        'hardwareswap': ['SSD','LPX','DDR2']
-        ,'buildapcsales':  ['SSD']
-        #,'steamGameSwap':  ['Bloodstained','No Man\'s Sky']
-        }
-        
+# enter the sub, then a list for the items you are looking for in the sub
+# any deal in a 'swap' reddit will only look in the HAVE section of the title
+deals = {
+    'hardwareswap': ['SSD', 'LPX', 'DDR2']
+    , 'buildapcsales': ['SSD']
+    # ,'steamGameSwap':  ['Bloodstained','No Man\'s Sky']
+}
+
 csvfilepath = '<ADD FILE PATH HERE>'
 
 try:
-    open(csvfilepath,'r').close()
-except IOError: 
-    open(csvfilepath,'w+').close()
-    
+    with open(csvfilepath, 'r') as csvFile:
+        seenList = [i.strip().split("|") for i in csvFile]
+        LoggedPostIDs = set({item[1] for item in seenList})
+except IOError:
+    LoggedPostIDs = set()
+    open(csvfilepath, 'w+').close()
 
 print('Starting Up...')
 while True:
-    #Create a blank frame that will be continiously dropped and reloaded 
-    # This is checking if we've already been alert of that deal
-    frame = []
     try:
-        with open(csvfilepath,'r+') as csvFile:
-            csvReader = csv.reader(csvFile,delimiter='|')
-            for line in csvReader:
-                frame.append(line)
-        LoggedPostIDs = [item[1] for item in frame]  
-        utctime = datetime.utcnow()  
         for k in deals:
             subreddit = reddit.subreddit(k)
             for submission in subreddit.new(limit=10):
+                postID = submission.id
+                if postID in LoggedPostIDs:
+                    continue
                 for i in deals[k]:
-                    #if a subreddit that is focused on users selling/trading something (i.e. r/hardwareswap, r/steamgameswap), only looking in the [H]ave Section
-                    if 'SWAP' in k.upper():
-                        if str(submission.title).find(i)<str(submission.title).find('[W]') and i in submission.title:
-                            postID = submission.id
-                            #Log the data to a file if it does not exist
-                            if postID not in LoggedPostIDs:
-                                with open(csvfilepath,'a',newline='\n') as csvFile:                        
-                                    csvWriter = csv.writer(csvFile,delimiter='|')
-                                    csvWriter.writerow([submission.subreddit
-                                                        ,submission.id
-                                                        ,submission.author
-                                                        ,datetime.fromtimestamp(submission.created_utc)
-                                                        ,submission.title
-                                                        ,submission.url])
-                                #Send Text Notification
-                                msgbody = str(i) + ' DEAL ALERT IN /r/' + str(submission.subreddit) + '\n' + submission.url
-                                SendTextMessage('<Phone Number here i.e. +18888888888>',msgbody)
-                                print('New Deal - Text Sent!')
-                            #else:
-                                #print('Already Notified')
-                    else:
-                        if i in submission.title:             
-                            postID = submission.id
-                            #Log the data to a file if it does not exist
-                            if postID not in LoggedPostIDs:
-                                with open(csvfilepath,'a',newline='\n') as csvFile:                        
-                                    csvWriter = csv.writer(csvFile,delimiter='|')
-                                    csvWriter.writerow([submission.subreddit
-                                                        ,submission.id
-                                                        ,submission.author
-                                                        ,datetime.fromtimestamp(submission.created_utc)
-                                                        ,submission.title
-                                                        ,submission.url])
-                                #Send Text Notification
-                                msgbody = str(i) + ' DEAL ALERT IN /r/' + str(submission.subreddit) + '\n' + submission.url
-                                SendTextMessage('<Phone Number here i.e. +18888888888>',msgbody)
-                                print('New Deal - Text Sent!')
-                            #else:
-                                #print('Already Notified')
+                    # if a subreddit that is focused on users selling/trading something (i.e. r/hardwareswap, r/steamgameswap), only looking in the [H]ave Section
+                    if i not in submission.title or \
+                            ('SWAP' in k.upper() and str(submission.title).find(i) > str(submission.title).find('[W]')):
+                        LoggedPostIDs.add(postID)
+                        continue
+
+                    # Log the data to a file if it does not exist
+                    with open(csvfilepath, 'a', newline='\n') as csvFile:
+                        csvWriter = csv.writer(csvFile, delimiter='|')
+                        csvWriter.writerow([submission.subreddit
+                                               , submission.id
+                                               , submission.author
+                                               , datetime.fromtimestamp(submission.created_utc)
+                                               , submission.title
+                                               , submission.url])
+                    # Send Text Notification
+                    msgbody = str(i) + ' DEAL ALERT IN /r/' + str(submission.subreddit) + '\n' + submission.url
+                    SendTextMessage('<Phone Number here i.e. +18888888888>', msgbody)
+                    print('New Deal - Text Sent!')
+                    LoggedPostIDs.add(postID)
+
+
     except KeyboardInterrupt:
         break
     except:
         print('Issue Connecting to reddit')
-    #wait 45 seconds; rinse and repeat
+    # wait 45 seconds; rinse and repeat
     sleep(45)
